f3e9ca1d701b1112be61f3f1c6bf5e617f1c757e
Coel625 <btaylor40@my.apsu.edu>
2018-04-28 03:18:53 -0500

    Created basic functionality for the similar explicit intents used in the schedule activities

--- app/src/main/java/edu/apsu/csci/goalsandschedules/ScheduleActivity.java
+++ app/src/main/java/edu/apsu/csci/goalsandschedules/ScheduleActivity.java
@@ -6,8 +6,22 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ScheduleActivity extends Activity implements View.OnClickListener {
+
+    private static final int SCHEDULE_FIELD1 = 1000;
+    private static final int SCHEDULE_FIELD2 = 2000;
+    private static final int SCHEDULE_FIELD3 = 3000;
+    private static final int SCHEDULE_FIELD4 = 4000;
+    private static final int SCHEDULE_FIELD5 = 5000;
+
+    public static final String SCHEDULEDATA1 = "scheduledata1";
+    public static final String SCHEDULEDATA2 = "scheduledata2";
+    public static final String SCHEDULEDATA3 = "scheduledata3";
+    public static final String SCHEDULEDATA4 = "scheduledata4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,21 @@ public class ScheduleActivity extends Activity implements View.OnClickListener {
 
         Button b4 = (Button) findViewById(R.id.scheduleadd_button);
         b4.setOnClickListener(this);
+
+        LinearLayout linearLayout1 = (LinearLayout) findViewById(R.id.scheduleEntry);
+        linearLayout1.setOnClickListener(this);
+
+        LinearLayout linearLayout2 = (LinearLayout) findViewById(R.id.scheduleEntry2);
+        linearLayout2.setOnClickListener(this);
+
+        LinearLayout linearLayout3 = (LinearLayout) findViewById(R.id.scheduleEntry3);
+        linearLayout3.setOnClickListener(this);
+
+        LinearLayout linearLayout4 = (LinearLayout) findViewById(R.id.scheduleEntry4);
+        linearLayout4.setOnClickListener(this);
+
+        LinearLayout linearLayout5 = (LinearLayout) findViewById(R.id.scheduleEntry5);
+        linearLayout5.setOnClickListener(this);
     }
 
     @Override
@@ -38,9 +67,229 @@ public class ScheduleActivity extends Activity implements View.OnClickListener {
         } else if (v.getId() == R.id.online_button) {
             Intent intent = new Intent(getApplicationContext(), OnlineActivity.class);
             startActivity(intent);
+        } else if (v.getId() == R.id.scheduleEntry) {
+            Intent intent = new Intent(getApplicationContext(), ScheduleCreationActivity.class);
+            startActivityForResult(intent, SCHEDULE_FIELD1);
+        } else if (v.getId() == R.id.scheduleEntry2) {
+            Intent intent = new Intent(getApplicationContext(), ScheduleCreationActivity.class);
+            startActivityForResult(intent, SCHEDULE_FIELD2);
+        } else if (v.getId() == R.id.scheduleEntry3) {
+            Intent intent = new Intent(getApplicationContext(), ScheduleCreationActivity.class);
+            startActivityForResult(intent, SCHEDULE_FIELD3);
+        } else if (v.getId() == R.id.scheduleEntry4) {
+            Intent intent = new Intent(getApplicationContext(), ScheduleCreationActivity.class);
+            startActivityForResult(intent, SCHEDULE_FIELD4);
+        } else if (v.getId() == R.id.scheduleEntry5) {
+            Intent intent = new Intent(getApplicationContext(), ScheduleCreationActivity.class);
+            startActivityForResult(intent, SCHEDULE_FIELD5);
         } else if (v.getId() == R.id.scheduleadd_button) {
             Intent intent = new Intent(getApplicationContext(), ScheduleCreationActivity.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == SCHEDULE_FIELD1) {
+            if (resultCode == RESULT_OK) {
+                String scheduleTimes = "";
+                if (data.hasExtra(SCHEDULEDATA1)) {
+                    scheduleTimes = data.getStringExtra(SCHEDULEDATA1);
+                }
+
+                String scheduleDate = "";
+                if (data.hasExtra(SCHEDULEDATA2)) {
+                    scheduleDate = data.getStringExtra(SCHEDULEDATA2);
+                }
+
+                String scheduleShort = "";
+                if (data.hasExtra(SCHEDULEDATA3)) {
+                    scheduleShort = data.getStringExtra(SCHEDULEDATA3);
+                }
+
+                String scheduleDesc = "";
+                if (data.hasExtra(SCHEDULEDATA4)) {
+                    scheduleDesc = data.getStringExtra(SCHEDULEDATA4);
+                }
+
+                TextView tv1 = (TextView) findViewById(R.id.scheduleEntryTimes);
+                tv1.setText(scheduleTimes);
+
+                TextView tv2 = (TextView) findViewById(R.id.scheduleEntryDate);
+                tv2.setText(scheduleDate);
+
+                TextView tv3 = (TextView) findViewById(R.id.scheduleEntryShort);
+                tv3.setText(scheduleShort);
+
+                TextView tv4 = (TextView) findViewById(R.id.scheduleEntryDesc);
+                tv4.setText(scheduleDesc);
+            }
+        } else if (resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, "Request cancelled", Toast.LENGTH_LONG).show();
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+        if (requestCode == SCHEDULE_FIELD2) {
+            if (resultCode == RESULT_OK) {
+                String scheduleTimes = "";
+                if (data.hasExtra(SCHEDULEDATA1)) {
+                    scheduleTimes = data.getStringExtra(SCHEDULEDATA1);
+                }
+
+                String scheduleDate = "";
+                if (data.hasExtra(SCHEDULEDATA2)) {
+                    scheduleDate = data.getStringExtra(SCHEDULEDATA2);
+                }
+
+                String scheduleShort = "";
+                if (data.hasExtra(SCHEDULEDATA3)) {
+                    scheduleShort = data.getStringExtra(SCHEDULEDATA3);
+                }
+
+                String scheduleDesc = "";
+                if (data.hasExtra(SCHEDULEDATA4)) {
+                    scheduleDesc = data.getStringExtra(SCHEDULEDATA4);
+                }
+
+                TextView tv1 = (TextView) findViewById(R.id.scheduleEntryTimes2);
+                tv1.setText(scheduleTimes);
+
+                TextView tv2 = (TextView) findViewById(R.id.scheduleEntryDate2);
+                tv2.setText(scheduleDate);
+
+                TextView tv3 = (TextView) findViewById(R.id.scheduleEntryShort2);
+                tv3.setText(scheduleShort);
+
+                TextView tv4 = (TextView) findViewById(R.id.scheduleEntryDesc2);
+                tv4.setText(scheduleDesc);
+            }
+        } else if (resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, "Request cancelled", Toast.LENGTH_LONG).show();
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+        if (requestCode == SCHEDULE_FIELD3) {
+            if (resultCode == RESULT_OK) {
+                String scheduleTimes = "";
+                if (data.hasExtra(SCHEDULEDATA1)) {
+                    scheduleTimes = data.getStringExtra(SCHEDULEDATA1);
+                }
+
+                String scheduleDate = "";
+                if (data.hasExtra(SCHEDULEDATA2)) {
+                    scheduleDate = data.getStringExtra(SCHEDULEDATA2);
+                }
+
+                String scheduleShort = "";
+                if (data.hasExtra(SCHEDULEDATA3)) {
+                    scheduleShort = data.getStringExtra(SCHEDULEDATA3);
+                }
+
+                String scheduleDesc = "";
+                if (data.hasExtra(SCHEDULEDATA4)) {
+                    scheduleDesc = data.getStringExtra(SCHEDULEDATA4);
+                }
+
+                TextView tv1 = (TextView) findViewById(R.id.scheduleEntryTimes3);
+                tv1.setText(scheduleTimes);
+
+                TextView tv2 = (TextView) findViewById(R.id.scheduleEntryDate3);
+                tv2.setText(scheduleDate);
+
+                TextView tv3 = (TextView) findViewById(R.id.scheduleEntryShort3);
+                tv3.setText(scheduleShort);
+
+                TextView tv4 = (TextView) findViewById(R.id.scheduleEntryDesc3);
+                tv4.setText(scheduleDesc);
+            }
+        } else if (resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, "Request cancelled", Toast.LENGTH_LONG).show();
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+        if (requestCode == SCHEDULE_FIELD4) {
+            if (resultCode == RESULT_OK) {
+                String scheduleTimes = "";
+                if (data.hasExtra(SCHEDULEDATA1)) {
+                    scheduleTimes = data.getStringExtra(SCHEDULEDATA1);
+                }
+
+                String scheduleDate = "";
+                if (data.hasExtra(SCHEDULEDATA2)) {
+                    scheduleDate = data.getStringExtra(SCHEDULEDATA2);
+                }
+
+                String scheduleShort = "";
+                if (data.hasExtra(SCHEDULEDATA3)) {
+                    scheduleShort = data.getStringExtra(SCHEDULEDATA3);
+                }
+
+                String scheduleDesc = "";
+                if (data.hasExtra(SCHEDULEDATA4)) {
+                    scheduleDesc = data.getStringExtra(SCHEDULEDATA4);
+                }
+
+                TextView tv1 = (TextView) findViewById(R.id.scheduleEntryTimes4);
+                tv1.setText(scheduleTimes);
+
+                TextView tv2 = (TextView) findViewById(R.id.scheduleEntryDate4);
+                tv2.setText(scheduleDate);
+
+                TextView tv3 = (TextView) findViewById(R.id.scheduleEntryShort4);
+                tv3.setText(scheduleShort);
+
+                TextView tv4 = (TextView) findViewById(R.id.scheduleEntryDesc4);
+                tv4.setText(scheduleDesc);
+            }
+        } else if (resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, "Request cancelled", Toast.LENGTH_LONG).show();
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+        if (requestCode == SCHEDULE_FIELD5) {
+            if (resultCode == RESULT_OK) {
+                String scheduleTimes = "";
+                if (data.hasExtra(SCHEDULEDATA1)) {
+                    scheduleTimes = data.getStringExtra(SCHEDULEDATA1);
+                }
+
+                String scheduleDate = "";
+                if (data.hasExtra(SCHEDULEDATA2)) {
+                    scheduleDate = data.getStringExtra(SCHEDULEDATA2);
+                }
+
+                String scheduleShort = "";
+                if (data.hasExtra(SCHEDULEDATA3)) {
+                    scheduleShort = data.getStringExtra(SCHEDULEDATA3);
+                }
+
+                String scheduleDesc = "";
+                if (data.hasExtra(SCHEDULEDATA4)) {
+                    scheduleDesc = data.getStringExtra(SCHEDULEDATA4);
+                }
+
+                TextView tv1 = (TextView) findViewById(R.id.scheduleEntryTimes5);
+                tv1.setText(scheduleTimes);
+
+                TextView tv2 = (TextView) findViewById(R.id.scheduleEntryDate5);
+                tv2.setText(scheduleDate);
+
+                TextView tv3 = (TextView) findViewById(R.id.scheduleEntryShort5);
+                tv3.setText(scheduleShort);
+
+                TextView tv4 = (TextView) findViewById(R.id.scheduleEntryDesc5);
+                tv4.setText(scheduleDesc);
+            }
+        } else if (resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, "Request cancelled", Toast.LENGTH_LONG).show();
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/edu/apsu/csci/goalsandschedules/ScheduleCreationActivity.java
+++ app/src/main/java/edu/apsu/csci/goalsandschedules/ScheduleCreationActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ScheduleCreationActivity extends Activity implements View.OnClickListener {
 
@@ -23,9 +27,6 @@ public class ScheduleCreationActivity extends Activity implements View.OnClickLi
         Button b = (Button) findViewById(R.id.schedulesubmit_button);
         b.setPaintFlags(b.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         b.setOnClickListener(this);
-
-        Button b2 = (Button) findViewById(R.id.newSTEntry_button);
-        b2.setOnClickListener(this);
     }
 
     @Override
@@ -33,22 +34,74 @@ public class ScheduleCreationActivity extends Activity implements View.OnClickLi
         //Button b = (Button) findViewById(R.id.schedulesubmit_button);
         //Button b2 = (Button) findViewById(R.id.newSTEntry_button);
         if (v.getId() == R.id.schedulesubmit_button) {
-            Intent intent = new Intent(getApplicationContext(), ScheduleActivity.class);
-            startActivity(intent);
-        } else if (v.getId() == R.id.newSTEntry_button) {
-            /*mainLayout.addView(createNewTextView("Temp"));
-            mainLayout.removeView(b2);
-            mainLayout.addView(b2);
-            mainLayout.removeView(b);
-            mainLayout.addView(b); */
+            finish();
         }
     }
 
-    /*private TextView createNewTextView(String text) {
-        final LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        final TextView textView = new TextView(this);
-        textView.setLayoutParams(lparams);
-        textView.setText("New text: " + text);
-        return textView;
-    } */
+    @Override
+    public void finish() {
+
+        Intent intent = new Intent();
+
+        EditText et1 = (EditText) findViewById(R.id.entryStartEdit);
+        EditText et2 = (EditText) findViewById(R.id.entryEndEdit);
+        EditText et3 = (EditText) findViewById(R.id.entryDescEdit);
+        Spinner spinner = (Spinner) findViewById(R.id.entryDateSelect);
+        Spinner spinner2 = (Spinner) findViewById(R.id.entryShortSelect);
+
+        String etString = et1.getText().toString();
+        String etString2 = et2.getText().toString();
+        String spinnerString = spinner.getSelectedItem().toString();
+        String spinnerString2 = spinner2.getSelectedItem().toString();
+        String etString3 = et3.getText().toString();
+
+        if (etString.equals("") || etString2.equals("") || etString3.equals("") ||
+                spinnerString.equals("Select a date:") || spinnerString2.equals("Select one of your short-term goals:")) {
+            Toast.makeText(this, "Please fill in the date and data fields for this entry", Toast.LENGTH_LONG).show();
+        } else {
+            if (!(etString.equals("")) || !(etString2.equals("")) || !(etString3.equals("")) ||
+                    !(spinnerString.equals("")) || !(spinnerString2.equals(""))) {
+                String combinedTimesString = "From " + etString + " to " + etString2;
+                intent.putExtra(ScheduleActivity.SCHEDULEDATA1, combinedTimesString);
+
+                String combinedDateString = "on " + spinnerString;
+                intent.putExtra(ScheduleActivity.SCHEDULEDATA2, combinedDateString);
+
+                String combinedShortString = "Short-Term Goals: " + spinnerString2;
+                intent.putExtra(ScheduleActivity.SCHEDULEDATA3, combinedShortString);
+
+                intent.putExtra(ScheduleActivity.SCHEDULEDATA4, etString3);
+
+                /*String combinedShortString = etString4 + ", " + etString6;
+                if (!(etString8.equals("")) && !(etString9.equals("")) && etString10.equals("") && etString11.equals("") &&
+                        etString12.equals("") && etString13.equals("")) {
+                    combinedShortString += ", " + etString8;
+                } else if (!(etString10.equals("")) && !(etString11.equals("")) && etString8.equals("") && etString9.equals("") &&
+                        etString12.equals("") && etString13.equals("")) {
+                    combinedShortString += ", " + etString10;
+                } else if (!(etString12.equals("")) && !(etString13.equals("")) && etString8.equals("") && etString9.equals("") &&
+                        etString10.equals("") && etString11.equals("")) {
+                    combinedShortString += ", " + etString12;
+                } else if (!(etString8.equals("")) && !(etString9.equals("")) && !(etString10.equals("")) && !(etString11.equals("")) &&
+                        etString12.equals("") && etString13.equals("")) {
+                    combinedShortString += ", " + etString8 + ", " + etString10;
+                } else if (!(etString8.equals("")) && !(etString9.equals("")) && !(etString12.equals("")) && !(etString13.equals("")) &&
+                        etString10.equals("") && etString11.equals("")) {
+                    combinedShortString += ", " + etString8 + ", " + etString12;
+                } else if (!(etString10.equals("")) && !(etString11.equals("")) && !(etString12.equals("")) && !(etString13.equals("")) &&
+                        etString8.equals("") && etString9.equals("")) {
+                    combinedShortString += ", " + etString10 + ", " + etString12;
+                } else if (!(etString8.equals("")) && !(etString9.equals("")) && !(etString10.equals("")) && !(etString11.equals("")) &&
+                        !(etString12.equals("")) && !(etString13.equals(""))) {
+                    combinedShortString += ", " + etString8 + ", " + etString10 + ", " + etString12;
+                } */
+
+                setResult(RESULT_OK, intent);
+            } else {
+                setResult(RESULT_CANCELED);
+            }
+
+            super.finish();
+        }
+    }
 }
