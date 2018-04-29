a4624ca149074af9581964c9ad753292f44b2820
WalkerC <christophermwalker@gmail.com>
2018-04-29 16:39:30 -0400

    DbDataSource Configured

--- app/src/main/java/edu/apsu/csci/goalsandschedules/MySqlLiteHelper.java
+++ app/src/main/java/edu/apsu/csci/goalsandschedules/MySqlLiteHelper.java
@@ -5,47 +5,49 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class MySqlLiteHelper extends SQLiteOpenHelper {
-    private static final String DB_NAME = "G&S.sqlite";
-    private static final int DB_VERSION = 1;
+    private static final String DB_NAME="G&S.sqlite";
+    private static final int DB_VERSION=1;
 
-    public static final String LONG_TERM_TABLE = "LongTerm";
-    public static final String SHORT_TERM_TABLE = "ShortTerm";
-    public static final String SCHEDULE_TABLE = "Schedule";
+    public static final String LONG_TERM_TABLE="LongTerm";
+    public static final String SHORT_TERM_TABLE="ShortTerm";
+    public static final String SCHEDULE_TABLE="Schedule";
 
     public enum LongTermColumns {
         longterm_id, title, description, progress;
 
-        public static String[] title() {
-            LongTermColumns[] v = values();
-            String[] title = new String[v.length];
-            for (int i = 0; i < v.length; i++) {
-                title[i] = v[i].toString();
+        public static String[] names() {
+            LongTermColumns[] v=values();
+            String[] names=new String[v.length];
+            for (int i=0; i < v.length; i++) {
+                names[i]=v[i].toString();
             }
-            return title;
+            return names;
         }
     }
-    public enum ShortTermColumns {
-        shortterm_id,longterm_id, title, description, order;
 
-        public static String[] title() {
-            ShortTermColumns[] v = values();
-            String[] title = new String[v.length];
-            for (int i = 0; i < v.length; i++) {
-                title[i] = v[i].toString();
+    public enum ShortTermColumns {
+        shortterm_id, longterm_id, title, description, order;
+
+        public static String[] names() {
+            ShortTermColumns[] v=values();
+            String[] names=new String[v.length];
+            for (int i=0; i < v.length; i++) {
+                names[i]=v[i].toString();
             }
-            return title;
+            return names;
         }
     }
+
     public enum ScheduleColumns {
         schedule_id, shortterm_id, title, date, start, end, description;
 
-        public static String[] title() {
-            ScheduleColumns[] v = values();
-            String[] title = new String[v.length];
-            for (int i = 0; i < v.length; i++) {
-                title[i] = v[i].toString();
+        public static String[] names() {
+            ScheduleColumns[] v=values();
+            String[] names=new String[v.length];
+            for (int i=0; i < v.length; i++) {
+                names[i]=v[i].toString();
             }
-            return title;
+            return names;
         }
     }
 
@@ -55,20 +57,20 @@ public class MySqlLiteHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "CREATE TABLE " + LONG_TERM_TABLE + " (" +
+        String sql="CREATE TABLE " + LONG_TERM_TABLE + " (" +
                 LongTermColumns.longterm_id + " INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , " +
                 LongTermColumns.title + " TEXT NOT NULL , " +
                 LongTermColumns.description + " TEXT NOT NULL , " +
                 LongTermColumns.progress + " INTEGER NOT NULL )";
         db.execSQL(sql);
-        sql = "CREATE TABLE " + SHORT_TERM_TABLE + " (" +
+        sql="CREATE TABLE " + SHORT_TERM_TABLE + " (" +
                 ShortTermColumns.shortterm_id + " INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , " +
                 ShortTermColumns.longterm_id + " INTEGER  NOT NULL , " +
                 ShortTermColumns.title + " TEXT NOT NULL , " +
                 ShortTermColumns.description + " TEXT NOT NULL ," +
                 ShortTermColumns.order + " INTEGER NOT NULL )";
         db.execSQL(sql);
-        sql = "CREATE TABLE " + SCHEDULE_TABLE + " (" +
+        sql="CREATE TABLE " + SCHEDULE_TABLE + " (" +
                 ScheduleColumns.schedule_id + " INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , " +
                 ScheduleColumns.shortterm_id + " INTEGER  NOT NULL , " +
                 ScheduleColumns.title + "  TEXT NOT NULL , " +
@@ -82,20 +84,20 @@ public class MySqlLiteHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (oldVersion == 1 && newVersion == 2) {
-            String sql = "alter table " + LONG_TERM_TABLE + " add column extra integer";
+            String sql="alter table " + LONG_TERM_TABLE + " add column extra integer";
             db.execSQL(sql);
 
-            sql = "update " + LONG_TERM_TABLE + " set extra = 42";
+            sql="update " + LONG_TERM_TABLE + " set extra = 42";
             db.execSQL(sql);
-            sql = "alter table " + SHORT_TERM_TABLE + " add column extra integer";
-            db.execSQL(sql);
-
-            sql = "update " + SHORT_TERM_TABLE + " set extra = 42";
-            db.execSQL(sql);
-            sql = "alter table " + SCHEDULE_TABLE + " add column extra integer";
+            sql="alter table " + SHORT_TERM_TABLE + " add column extra integer";
             db.execSQL(sql);
 
-            sql = "update " + SCHEDULE_TABLE + " set extra = 42";
+            sql="update " + SHORT_TERM_TABLE + " set extra = 42";
+            db.execSQL(sql);
+            sql="alter table " + SCHEDULE_TABLE + " add column extra integer";
+            db.execSQL(sql);
+
+            sql="update " + SCHEDULE_TABLE + " set extra = 42";
             db.execSQL(sql);
         }
     }
@@ -110,11 +112,11 @@ public class MySqlLiteHelper extends SQLiteOpenHelper {
                 db.beginTransaction();
 
                 // copy table that needs column dropped
-                String sql = "alter table " + LONG_TERM_TABLE + " rename to tmp";
+                String sql="alter table " + LONG_TERM_TABLE + " rename to tmp";
                 db.execSQL(sql);
 
                 // recreate the table in the old schema
-                sql = "CREATE TABLE " + LONG_TERM_TABLE + " (" +
+                sql="CREATE TABLE " + LONG_TERM_TABLE + " (" +
                         LongTermColumns.longterm_id + " INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , " +
                         LongTermColumns.title + " TEXT NOT NULL , " +
                         LongTermColumns.description + " TEXT NOT NULL , " +
@@ -122,7 +124,7 @@ public class MySqlLiteHelper extends SQLiteOpenHelper {
                 db.execSQL(sql);
 
                 // copy the data we want from the old table
-                sql = "insert into " + LONG_TERM_TABLE +
+                sql="insert into " + LONG_TERM_TABLE +
                         " select " +
                         LongTermColumns.longterm_id + ", " +
                         LongTermColumns.title + ", " +
@@ -131,11 +133,11 @@ public class MySqlLiteHelper extends SQLiteOpenHelper {
                         " from tmp";
                 db.execSQL(sql);
                 // copy table that needs column dropped
-                sql = "alter table " + SHORT_TERM_TABLE + " rename to tmp";
+                sql="alter table " + SHORT_TERM_TABLE + " rename to tmp";
                 db.execSQL(sql);
 
                 // recreate the table in the old schema
-                sql = "CREATE TABLE " + SHORT_TERM_TABLE + " (" +
+                sql="CREATE TABLE " + SHORT_TERM_TABLE + " (" +
                         ShortTermColumns.shortterm_id + " INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , " +
                         ShortTermColumns.longterm_id + " INTEGER  NOT NULL , " +
                         ShortTermColumns.title + " TEXT NOT NULL , " +
@@ -144,21 +146,21 @@ public class MySqlLiteHelper extends SQLiteOpenHelper {
                 db.execSQL(sql);
 
                 // copy the data we want from the old table
-                sql = "insert into " + SHORT_TERM_TABLE +
+                sql="insert into " + SHORT_TERM_TABLE +
                         " select " +
                         ShortTermColumns.shortterm_id + ", " +
                         ShortTermColumns.longterm_id + ", " +
                         ShortTermColumns.title + ", " +
-                        ShortTermColumns.description +", " +
+                        ShortTermColumns.description + ", " +
                         ShortTermColumns.order +
                         " from tmp";
                 db.execSQL(sql);
                 // copy table that needs column dropped
-                sql = "alter table " + SCHEDULE_TABLE + " rename to tmp";
+                sql="alter table " + SCHEDULE_TABLE + " rename to tmp";
                 db.execSQL(sql);
 
                 // recreate the table in the old schema
-                sql = "CREATE TABLE " + SCHEDULE_TABLE + " (" +
+                sql="CREATE TABLE " + SCHEDULE_TABLE + " (" +
                         ScheduleColumns.schedule_id + " INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , " +
                         ScheduleColumns.shortterm_id + " INTEGER  NOT NULL , " +
                         ScheduleColumns.title + "  TEXT NOT NULL , " +
@@ -169,7 +171,7 @@ public class MySqlLiteHelper extends SQLiteOpenHelper {
                 db.execSQL(sql);
 
                 // copy the data we want from the old table
-                sql = "insert into " + SCHEDULE_TABLE +
+                sql="insert into " + SCHEDULE_TABLE +
                         " select " +
                         ScheduleColumns.schedule_id + ", " +
                         ScheduleColumns.shortterm_id + ", " +
@@ -182,19 +184,16 @@ public class MySqlLiteHelper extends SQLiteOpenHelper {
                 db.execSQL(sql);
 
                 // get rid of the temporary table
-                sql = "drop table tmp";
+                sql="drop table tmp";
                 db.execSQL(sql);
 
                 db.setTransactionSuccessful();
-            }  catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             } finally {
                 db.endTransaction();
             }
-
-
         }
-
     }
 }
 
